3dbaa19307084b0a0e71a925ad670a528c017e47
webkcom <webkcom@outlook.com>
2022-05-14 23:29:07 +0800

    chore: add website logo

---
+++ src/app/layout/header/logo/logo.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.less'],
+})
+
+export class LogoComponent implements OnInit {
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+
+}
