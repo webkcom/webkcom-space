f50bc314730df8cb6268fadf863ddb234a030916
webkcom <webkcom@outlook.com>
2022-05-17 22:08:53 +0800

    feat: add HostListener to get the current window size

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, HostListener, Input, OnChanges, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +7,25 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 
 export class HeaderComponent implements OnInit {
+  crowded = false;
+  date!:number;
+  // 获取当前窗口大小
+  scrWidth!: number;
+  @HostListener('window:resize', ['$event'])
+  getScreenSize() {
+    this.scrWidth = window.innerWidth;
+    if (this.scrWidth < 1200) {
+      this.crowded = true;
+    }
+    else {
+      this.crowded = false;
+    }
+    // console.log(this.crowded);
+  }
+
   constructor() {
-
   }
-
   ngOnInit(): void {
-
+    this.getScreenSize();
   }
-
-
 }
