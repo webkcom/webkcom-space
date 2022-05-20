0f2dfda83c28642863914ece373e876571509e0f
webkcom <webkcom@outlook.com>
2022-05-20 12:41:14 +0800

    chore: remove "crowded" varaible and associated code

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -7,19 +7,12 @@ import { ChangeDetectorRef, Component, HostListener, Input, OnChanges, OnInit, S
 })
 
 export class HeaderComponent implements OnInit {
-  crowded = false;
   date!:number;
   // 获取当前窗口大小
   scrWidth!: number;
   @HostListener('window:resize', ['$event'])
   getScreenSize() {
     this.scrWidth = window.innerWidth;
-    if (this.scrWidth < 1200) {
-      this.crowded = true;
-    }
-    else {
-      this.crowded = false;
-    }
     // console.log(this.crowded);
   }
 
